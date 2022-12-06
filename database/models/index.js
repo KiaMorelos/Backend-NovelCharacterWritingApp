64e7593140609292250d6e401f2056acbb0a556f
KiaMorelos <kiana.morelos@gmail.com>
2022-12-05 23:10:33 -0700

    fixed test database being in the wrong place

--- database/models/index.js
+++ database/models/index.js
@@ -12,7 +12,7 @@ let sequelize;
 
 if (process.env.NODE_ENV === "development") {
   sequelize = new Sequelize(process.env.DEV_DATABASE_URL);
-} else if (process.env.NODE_ENV === "development") {
+} else if (process.env.NODE_ENV === "test") {
   sequelize = new Sequelize(process.env.TEST_DATABASE_URL);
 } else {
   sequelize = new Sequelize(process.env.DATABASE_URL);
